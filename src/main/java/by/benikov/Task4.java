0830ee036d226d0cc61845b5e775e062b21ac07b
ilyabenikov <vatnaya1997@gmail.com>
2022-02-08 23:42:08 +0300

    Rewrite Task4.java

--- src/main/java/by/benikov/Task4.java
+++ src/main/java/by/benikov/Task4.java
@@ -2,13 +2,12 @@ package by.benikov;
 
 public class Task4 {
     public static void main(String[] args) {
-        int x = -5, y = -3;
+        int x = 1, y = 1;
         System.out.println(isBelong(x, y));
     }
 
     public static boolean isBelong(int x, int y){
-        if((x >= -4 && x <= 4) && (y <= 2 && y >= -3)) return true;
-        if((y <= 4 && y >= -3) && (x >= -2 && x <= 2)) return true;
-        return false;
+        return (((x >= -4 && x <= 4) && (y <= 2 && y >= -3)) &&
+                ((y <= 4 && y >= -3) && (x >= -2 && x <= 2)));
     }
 }
